6bd038bb5fd37940aba11251a227ac850dbeb37e
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2021-10-13 21:13:35 -0300

    Add project files.

---
+++ Estoque.MVC/Models/EPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estoque.MVC.Models
+{
+    public class EPI : Produto
+    {
+        public int CA { get; set; }
+    }
+}
---
+++ Estoque.MVC/Models/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estoque.MVC.Models
+{
+    public class Funcionario
+    {
+        public int Registro { get; set; }
+        public string Nome { get; set; }
+        public string Funcao { get; set; }
+    }
+}
